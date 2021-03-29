0b15942c99674b310651b07949dfc22812439823
Dineshkumar510 <dk6089567@gmail.com>
2021-03-29 22:18:53 +0530

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unused-vars */
+import React from "react";
 import './App.css';
-
+import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from "@syncfusion/ej2-react-schedule";
+import {DataManager, WebApiAdaptor} from "@syncfusion/ej2-data";
 function App() {
+
+  {/*var localdata = EventSettingsModel = {
+    dataSource : [{
+      EndTime: new Date(2020, 0, 11, 6, 30),
+      StartTime: new Date(2021, 4, 11, 4, 0)
+    }]
+  };*/}
+
+  var remoteData = new DataManager ({
+    url: 'https://js.syncfusion.com/demos/ejservices/api/schedule/LoadData',
+    adaptor : new WebApiAdaptor(),
+    crossDomain: true
+  }); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ScheduleComponent currentView='Month' selectedDate={new Date(2021, 2, 18)}
+        /*eventSettings = {this.localdata}*/>
+          <Inject services = {[Day, Week, WorkWeek, Month, Agenda]}/>
+        </ScheduleComponent>
     </div>
   );
 }
